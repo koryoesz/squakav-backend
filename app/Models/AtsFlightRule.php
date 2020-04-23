e4d81e852830fd52922a65681d457dfd25464666
koryoesz <koryoesz@gmail.com>
2020-04-23 22:35:15 +0100

    adding models

---
+++ app/Models/AtsFlightRule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AtsFlightRule extends Model
+{
+    protected $table = "ats_flight_rules";
+
+}
---
+++ app/Models/WakeTurbulenceCategory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 4/23/2020
+ * Time: 10:30 PM
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class WakeTurbulenceCategory extends Model
+{
+
+}

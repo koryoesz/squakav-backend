59e8d7eb5c50d28e304140d09e2cc25855b07572
koryoesz <koryoesz@gmail.com>
2020-04-30 00:22:19 +0100

    cache ats flight rules for faster data retrieval

--- app/Services/AtsFlightRuleService.php
+++ app/Services/AtsFlightRuleService.php
@@ -9,13 +9,20 @@
 namespace App\Services;
 
 use App\Models\AtsFlightRule;
+use Illuminate\Support\Facades\Cache;
 
 
 class AtsFlightRuleService
 {
     public function getAllAtsFlightRule()
     {
-        $rules = AtsFlightRule::all();
+        $rules = Cache::get('ats_flight_rules');
+
+        if(empty($rules))
+        {
+            $rules = AtsFlightRule::all();
+            Cache::put('ats_flight_rules', $rules, 18000);
+        }
         return $rules;
     }
 }

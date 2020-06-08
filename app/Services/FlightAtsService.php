ff1b4509abeb6c23eede1b7b81b2b323550c3bec
koryoesz <koryoesz@gmail.com>
2020-06-08 15:28:04 +0100

    fix get one sent

--- app/Services/FlightAtsService.php
+++ app/Services/FlightAtsService.php
@@ -188,7 +188,7 @@ class FlightAtsService
      */
     public function getOneSent($id)
     {
-        $flight = FlightAts::where('id', $id)->where('status_id', Status::ACTIVE);
+        $flight = FlightAts::where('id', $id)->where('status_id', Status::ACTIVE)->get();
         return $flight;
     }
 

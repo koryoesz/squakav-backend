72daf8127968b1cb4a89a09320ab4755bd3dc981
koryoesz <koryoesz@gmail.com>
2020-06-10 16:42:03 +0100

    validation component on flightatsService

---
+++ app/Components/EaseFlightValidation.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 6/10/2020
+ * Time: 4:27 PM
+ */
+
+namespace App\Components;
+
+use Illuminate\Support\Facades\Validator;
+
+class EaseFlightValidation
+{
+    /**
+     * @param $params
+     */
+    public static function validate($params)
+    {
+        $validator = Validator::make($params, [
+            'aircraft_identification' => 'required|string|max:7',
+            'ats_flight_rules_id' => 'sometimes|numeric|exists:ats_flight_rules,id',
+            'aircraft_type' => 'sometimes|required|string|min:2|max:4',
+            'wake_turbulence_category_id' => 'sometimes|exists:wake_turbulence_category,id',
+            'departure' => 'sometimes|string|min:3|max:4',
+            'cruising_speed' => 'sometimes|string|min:4|max:5',
+            'level' => 'sometimes|string|min:4|max:5',
+            'route' => 'sometimes|string|max:128',
+            'destination' => 'sometimes|string|min:3|max:4',
+            'total_eet' => 'sometimes|numeric|digits:4',
+            'alternate_one' => 'sometimes|string|min:3|max:4',
+            'alternate_two' => 'sometimes|string|min:3|max:4',
+            'endurance' => 'sometimes|numeric|digits:4',
+            'persons_on_board' => 'sometimes|string|min:3|max:3',
+            'filed_by' => 'sometimes|string|max:128',
+            'color_markings' => 'sometimes|string|max:128',
+            'pilot_in_command' => 'sometimes|string|max:128',
+            'flight_type_id' => 'sometimes|numeric|exists:flight_types,id',
+            'time' => 'sometimes|digits:4',
+            'number' => 'sometimes|digits:2',
+            'remarks' => 'sometimes|required',
+        ]);
+
+        return $validator;
+    }
+
+    /**
+     * @param $params
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public static function forceValidate($params)
+    {
+        $validator = Validator::make($params, [
+            'aircraft_identification' => 'required|string|max:7',
+            'ats_flight_rules_id' => 'required|numeric|exists:ats_flight_rules,id',
+            'aircraft_type' => 'sometimes|required|string|min:2|max:4',
+            'wake_turbulence_category_id' => 'exists:wake_turbulence_category,id',
+            'departure' => 'required|string|min:3|max:4',
+            'cruising_speed' => 'required|string|min:4|max:5',
+            'level' => 'required|string|min:4|max:5',
+            'route' => 'required|string|max:128',
+            'destination' => 'required|string|min:3|max:4',
+            'total_eet' => 'required|numeric|digits:4',
+            'alternate_one' => 'required|string|min:3|max:4',
+            'alternate_two' => 'sometimes|required|string|min:3|max:4',
+            'endurance' => 'required|numeric|digits:4',
+            'persons_on_board' => 'required|string|min:3|max:3',
+            'filed_by' => 'required|string|max:128',
+            'color_markings' => 'required|string|max:128',
+            'pilot_in_command' => 'required|string|max:128',
+            'flight_type_id' => 'required|numeric|exists:flight_types,id',
+            'time' => 'required|digits:4',
+            'number' => 'sometimes|digits:2',
+            'remarks' => 'sometimes|required',
+        ]);
+
+        return $validator;
+    }
+
+}
--- app/Services/FlightAtsService.php
+++ app/Services/FlightAtsService.php
@@ -20,6 +20,7 @@ use App\Components\Exception as MyException;
 use App\Components\ErrorCode;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Validation\Rule;
+use App\Components\EaseFlightValidation;
 
 class FlightAtsService
 {
@@ -72,29 +73,7 @@ class FlightAtsService
             throw new MyException('Could not find system config', ErrorCode::INTERNAL_ERROR);
         }
 
-        $validator = Validator::make($params, [
-            'aircraft_identification' => 'required|string|max:7',
-            'ats_flight_rules_id' => 'required|numeric|exists:ats_flight_rules,id',
-            'aircraft_type' => 'sometimes|required|string|min:2|max:4',
-            'wake_turbulence_category_id' => 'exists:wake_turbulence_category,id',
-            'departure' => 'required|string|min:3|max:4',
-            'cruising_speed' => 'required|string|min:4|max:5',
-            'level' => 'required|string|min:4|max:5',
-            'route' => 'required|string|max:128',
-            'destination' => 'required|string|min:3|max:4',
-            'total_eet' => 'required|numeric|digits:4',
-            'alternate_one' => 'required|string|min:3|max:4',
-            'alternate_two' => 'sometimes|required|string|min:3|max:4',
-            'endurance' => 'required|numeric|digits:4',
-            'persons_on_board' => 'required|string|min:3|max:3',
-            'filed_by' => 'required|string|max:128',
-            'color_markings' => 'required|string|max:128',
-            'pilot_in_command' => 'required|string|max:128',
-            'flight_type_id' => 'required|numeric|exists:flight_types,id',
-            'time' => 'required|digits:4',
-            'number' => 'sometimes|digits:2',
-            'remarks' => 'sometimes|required',
-        ]);
+        $validator = EaseFlightValidation::forceValidate($params);
 
         throw_if($validator->fails(), ValidationException::class, $validator->errors());
 
@@ -186,29 +165,7 @@ class FlightAtsService
             throw new MyException('Could not find system config', ErrorCode::INTERNAL_ERROR);
         }
 
-        $validator = Validator::make($params, [
-            'aircraft_identification' => 'required|string|max:7',
-            'ats_flight_rules_id' => 'sometimes|numeric|exists:ats_flight_rules,id',
-            'aircraft_type' => 'sometimes|required|string|min:2|max:4',
-            'wake_turbulence_category_id' => 'sometimes|exists:wake_turbulence_category,id',
-            'departure' => 'sometimes|string|min:3|max:4',
-            'cruising_speed' => 'sometimes|string|min:4|max:5',
-            'level' => 'sometimes|string|min:4|max:5',
-            'route' => 'sometimes|string|max:128',
-            'destination' => 'sometimes|string|min:3|max:4',
-            'total_eet' => 'sometimes|numeric|digits:4',
-            'alternate_one' => 'sometimes|string|min:3|max:4',
-            'alternate_two' => 'sometimes|string|min:3|max:4',
-            'endurance' => 'sometimes|numeric|digits:4',
-            'persons_on_board' => 'sometimes|string|min:3|max:3',
-            'filed_by' => 'sometimes|string|max:128',
-            'color_markings' => 'sometimes|string|max:128',
-            'pilot_in_command' => 'sometimes|string|max:128',
-            'flight_type_id' => 'sometimes|numeric|exists:flight_types,id',
-            'time' => 'sometimes|digits:4',
-            'number' => 'sometimes|digits:2',
-            'remarks' => 'sometimes|required',
-        ]);
+        $validator = EaseFlightValidation::validate($params);
 
         throw_if($validator->fails(), ValidationException::class, $validator->errors());
 
@@ -315,9 +272,10 @@ class FlightAtsService
     public function approve($user_id, $params)
     {
         // include routes
-        $extra_param = ['user_id' => $user_id,
-                        'remarks' => isset($params['remarks']) ? $params['remarks']: '',
-                        'flight_id' => isset($params['flight_id']) ? $params['flight_id']: ''
+        // time and dates accepted
+        $extra_param = [
+                            'user_id' => $user_id,
+                            'flight_id' => isset($params['flight_id']) ? $params['flight_id']: ''
                         ];
 
         $validator = Validator::make($extra_param, [
@@ -326,7 +284,6 @@ class FlightAtsService
                 Rule::exists('ais_users', 'id')
                 ->where('user_type_id', UserType::AIS_USER_TYPE_ID)
                 ],
-            'remarks' => 'required',
             'flight_id' => [
                     'required',
                     'numeric',
@@ -385,7 +342,7 @@ class FlightAtsService
     public function decline($user_id, $params)
     {
         $extra_param = ['user_id' => $user_id,
-            'remarks' => isset($params['remarks']) ? $params['remarks']: '',
+            'additional_requirement' => isset($params['additional_requirement']) ? $params['additional_requirement']: '',
             'flight_id' => isset($params['flight_id']) ? $params['flight_id']: ''
         ];
 
@@ -403,13 +360,11 @@ class FlightAtsService
                     ->where('system_flight_types_id', SystemFightType::ATS)
             ]
         ], [
-            'user_id.exists' => 'User not authorized to approve ATS flight',
+            'user_id.exists' => 'User not authorized to decline ATS flight',
             'flight_id.exists' => 'Invalid Flight'
         ]);
 
-        // validator
-        // fails
-        // where is it ???/??????
+        throw_if($validator->fails(), ValidationException::class, $validator->errors());
 
         $flight = FlightAts::find($params['flight_id']);
         $system_flight = SystemFlight::where('flight_id', $params['flight_id'])->get();
@@ -420,11 +375,24 @@ class FlightAtsService
 
         $system_flight[0]->status_id = Status::DECLINED;
         $flight->status_id = Status::DECLINED;
+        $flight->additional_requirement = $params['additional_requirement'];
 
         $system_flight[0]->save();
         $flight->save();
 
         return "ATS Flight Plan Has Been Declined.";
+    }
+
+    /**
+     * @param $user_id
+     * @param $flight_id
+     * @param $params
+     */
+    public function updateDraft($user_id, $flight_id, $params)
+    {
+        $validator = EaseFlightValidation::validate($params);
+        throw_if($validator->fails(), ValidationException::class, $validator->errors());
+
     }
 
 }

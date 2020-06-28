fe82941098cda41736702152655efc91cccc3b3b
koryoesz <koryoesz@gmail.com>
2020-06-28 05:39:21 +0100

    .'

--- database/migrations/2020_05_08_111035_create_rpl_flight_days.php
+++ database/migrations/2020_05_08_111035_create_rpl_flight_days.php
@@ -14,6 +14,8 @@ class CreateRplFlightDays extends Migration
     public function up()
     {
         Schema::create('flight_rpl_days', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            
             $table->bigIncrements('id');
             $table->unsignedSmallInteger('status_id')->default(1);
             $table->boolean('monday')->default(0);

19b9b1f0613eab62502a35f5ac39109054ed3cb3
koryoesz <koryoesz@gmail.com>
2020-08-10 22:27:08 +0100

    .

--- app/Services/SystemFlightService.php
+++ app/Services/SystemFlightService.php
@@ -816,14 +816,14 @@ class SystemFlightService
                     $temp_flight->destination_airport_id == $user->airport->id
                     && $temp_flight->flight_date == $today)
                 {
-                    $flights[] = $temp_flight;
+                    $flights[] = $temp_flight->with('operator')->get();
                 }
                 else{
                     if(isset($temp_flight->destination)
                         && $temp_flight->destination == $user->airport->icao_code
                         && $temp_flight->flight_date == $today
                     ){
-                        $flights[] = $temp_flight;
+                        $flights[] = $temp_flight->with('operator')->get();
                     }
                 }
             }
@@ -876,13 +876,13 @@ class SystemFlightService
                     $temp_flight->departure_airport_id == $user->airport->id
                     && $temp_flight->flight_date == $today)
                 {
-                    $flights[] = $temp_flight->with('operator');
+                    $flights[] = $temp_flight;
                 }
                 else{
                     if(isset($temp_flight->destination)
                         && $temp_flight->destination == $user->airport->icao_code
                         && $temp_flight->flight_date == $today){
-                        $flights[] = $temp_flight->with('operator');
+                        $flights[] = $temp_flight;
                     }
                 }
             }
